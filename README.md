785beb77b04b23e3d6ebf4362cf7fea716c0d777
s33ding <robertomdiniz@protonmail.com>
2024-06-13 03:54:51 +0000

    saving

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# OSINTube-RealTimeGuard
+OSINTube-RealTimeGuard
 
-OSINTube-RealTimeGuard is a real-time threat detection system for YouTube content. It leverages AWS YouTube Search and PyTube for efficient video retrieval and transcription. With continuous monitoring and analysis, this system ensures online safety by identifying potential threats and suspicious activities in YouTube videos.
+OSINTube-RealTimeGuard is a real-time threat detection system for YouTube content. It ensures online safety by identifying potential threats and suspicious activities in YouTube videos through continuous monitoring and analysis.
+
+Features
+
+    Google API Client: Scrapes YouTube comments and translates them.
+    YouTubeSearchPython: Searches for videos on YouTube.
+    Amazon S3: Stores retrieved data.
+    NLP: Tokenizes the text.
+    OpenAI: Performs sentiment analysis.
+    Streamlit: Provides the backend interface.
+    Docker: Containerizes the application for easy deployment.
+
+![Project Logo]("app/media/osintube.webp")
--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
-comments_maxResult = 20
-comments_maxResult = 15
+comments_maxResult = 25
+comments_maxResult = 80
 gcp_api_key =  os.environ["GCP_CRED"]
 openai_api_key =  os.environ["OPENAI_CRED"]
 aws_key =  os.environ["AWS_ACCESS_KEY_ID"]
@@ -14,5 +14,7 @@ output_path = f"output/{default_file_name}"
 delete_file = False
 img_path = "media/osintube.webp"
 
-readme = f"""OSINTube-RealTimeGuard is a real-time threat detection system for YouTube content. It leverages AWS YouTube Search and PyTube for efficient video retrieval and transcription. With continuous monitoring and analysis, this system ensures online safety by identifying potential threats and suspicious activities in YouTube videos."""
+readme = f"""
+OSINTube-RealTimeGuard is a real-time threat detection system for YouTube content, ensuring online safety by identifying threats and suspicious activities through continuous monitoring and analysis. It leverages the Google API Client to scrape and translate YouTube comments, YouTubeSearchPython to search for videos, Amazon S3 for data storage, NLP for text tokenization, OpenAI for sentiment analysis, Streamlit for the backend interface, and Docker for easy deployment.
+"""
 
